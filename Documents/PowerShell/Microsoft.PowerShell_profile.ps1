d73b7cc3869288222f6e580805d1bf3b541a8114
Jacob Vaverka <jvaverka@protonmail.com>
2022-02-11 16:31:09 -0500

    modify banner

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 Clear-Host
+neofetch
 Write-Host 'Here we go 🚀'
-Write-Host 'PowerShell' $PsVersionTable.PSVersion '-' (Get-date)
-
+Write-Host 'PowerShell' $PsVersionTable.PSVersion
+Write-Host (Get-date)
+'-------------------------------------------------'
 . $env:USERPROFILE\.config\powershell\user_profile.ps1
